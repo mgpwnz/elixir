d3ca7c8e6bbb386ccfdda2f6243535ec2d422b0d
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-11-09 16:22:52 +0200

    update

--- install.sh
+++ install.sh
@@ -23,32 +23,6 @@ while test $# -gt 0; do
 	esac
 done
 install() {
-#docker install
-cd
-touch $HOME/.bash_profile
-if ! docker --version; then
-		echo -e "${C_LGn}Docker installation...${RES}"
-		sudo apt update
-		sudo apt upgrade -y
-		sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
-		. /etc/*-release
-		wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-		echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-		sudo apt update
-		sudo apt install docker-ce docker-ce-cli containerd.io -y
-		docker_version=`apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)"`
-		sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
-	fi
-	if ! docker-compose --version; then
-		echo -e "${C_LGn}Docker Сompose installation...${RES}"
-		sudo apt update
-		sudo apt upgrade -y
-		sudo apt install wget jq -y
-		local docker_compose_version=`wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name"`
-		sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
-		sudo chmod +x /usr/bin/docker-compose
-		. $HOME/.bash_profile
-	fi
 cd $HOME
 mkdir $HOME/elixir && cd $HOME/elixir
 wget -q O- Dockerfile https://files.elixir.finance/Dockerfile 
