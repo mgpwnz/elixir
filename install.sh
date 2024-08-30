fd73c55dfae585e38f75d5e7755cc4acee43cdb8
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-08-30 11:23:41 +0300

    updt

--- install.sh
+++ install.sh
@@ -13,50 +13,112 @@ while test $# -gt 0; do
             function="uninstall"
             shift
             ;;
-	    -up|--update)
-            function="update"
-            shift
-            ;;
         *|--)
-		break
-		;;
+    break
+	;;
 	esac
 done
 install() {
+#docker install
 cd $HOME
-mkdir $HOME/elixir && cd $HOME/elixir
-wget -q O- Dockerfile https://files.elixir.finance/Dockerfile 
-read -p "Enter wallet address: " EVM_WALLET_ADDRESS
-sed -i -e "s%ENV ADDRESS=0x.*%ENV ADDRESS=$EVM_WALLET_ADDRESS%g" $HOME/elixir/Dockerfile
-read -p "Enter Private Key: " EVM_PK
-sed -i -e "s%ENV PRIVATE_KEY=0x.*%ENV PRIVATE_KEY=0x$EVM_PK%g" $HOME/elixir/Dockerfile
-read -p "Enter Validator Name: " Name
-sed -i -e "s%ENV VALIDATOR_NAME=AnonValidator.*%ENV VALIDATOR_NAME=$Name%g" $HOME/elixir/Dockerfile
-docker build . -f Dockerfile -t elixir-validator &&\
-docker run -d --restart unless-stopped --name ev elixir-validator
-sleep 2
-cd
-echo Installed!
+. <(wget -qO- https://raw.githubusercontent.com/mgpwnz/VS/main/docker.sh)
+#create dir and config
+if [ ! -d $HOME/elixir ]; then
+  mkdir $HOME/elixir
+fi
+sleep 1
 
+function check_empty {
+  local varname=$1
+  while [ -z "${!varname}" ]; do
+    read -p "$2" input
+    if [ -n "$input" ]; then
+      eval $varname=\"$input\"
+    else
+      echo "The value cannot be empty. Please try again."
+    fi
+  done
+}
+
+function confirm_input {
+  echo "You have entered the following information:"
+  echo "Node Name: $NAME"
+  echo "Wallet Adress: $WA"
+  echo "Private Key: $PK"
+  
+  read -p "Is this information correct? (yes/no): " CONFIRM
+  if [ "$CONFIRM" != "yes" ]; then
+    echo "Let's try again..."
+    return 1 
+  fi
+  return 0 
+}
+
+while true; do
+  NAME=""
+  WA=""
+  PK=""
+  
+  check_empty NAME "Enter node NAME: "
+  check_empty WA "Wallet Adress: "
+  check_empty PK "Private Key: "
+  
+  confirm_input
+  if [ $? -eq 0 ]; then
+    break 
+  fi
+done
+
+echo "All data is confirmed. Proceeding..."
+
+# Create script 
+tee $HOME/elixir/docker-compose.yml > /dev/null <<EOF
+version: "3.7"
+name: elixir
+
+services:
+  node:
+    image: elixirprotocol/validator:3.1.0
+    restart: always
+    env_file:
+      - ./.env
+    ports:
+    - '17690:17690'
+networks:
+  default:
+    driver: bridge
+    ipam:
+      config:
+        - subnet: 192.168.200.0/24
+
+EOF
+#env
+tee $HOME/chasm/.env > /dev/null <<EOF
+ENV=testnet-3
+
+STRATEGY_EXECUTOR_IP_ADDRESS=`wget -qO- eth0.me`
+STRATEGY_EXECUTOR_DISPLAY_NAME=$NAME
+STRATEGY_EXECUTOR_BENEFICIARY=$WA
+SIGNER_PRIVATE_KEY=$PK
+EOF
+#Run nnode
+docker compose -f $HOME/elixir/docker-compose.yml up -d
 }
 uninstall() {
-cd $HOME
-docker kill ev &&\
-docker rm ev &&\
+if [ ! -d "$HOME/elixir" ]; then
+    break
+fi
+read -r -p "Wipe all DATA? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+docker compose -f $HOME/elixir/docker-compose.yml down -v
 rm -rf $HOME/elixir
-cd $HOME
-echo Remove!
-}
-update() {
-sudo apt update &> /dev/null
-cd $HOME/elixir
-docker kill ev
-docker rm ev
-docker pull elixirprotocol/validator:testnet-2
-docker build . -f Dockerfile -t elixir-validator &&\
-docker run -d --restart unless-stopped --name ev elixir-validator
-cd $HOME
-echo Update!
+        ;;
+    *)
+	echo Canceled
+	break
+        ;;
+esac
 }
 # Actions
 sudo apt install wget -y &>/dev/null
